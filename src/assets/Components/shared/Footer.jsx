4c5616517cfe5cb206f4c58401b2eecdd0139def
mjaabdullah <mdjuelahmedabdullah@gmail.com>
2026-04-12 22:11:56 +0600

    Add footer

---
+++ src/assets/Components/shared/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="border-t border-base-200 py-6 bg-gray-800  mt-8">
+      <div className="max-w-7xl mx-auto flex flex-col">
+        <p className="text-center text-xl text-gray-300">
+          &copy; 2025 BookVibe. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/assets/layout/MainLayout.jsx
+++ src/assets/layout/MainLayout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router";
+import Footer from "../Components/shared/Footer";
 import Navbar from "../Components/shared/Navbar";
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
-      <Outlet />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 };
