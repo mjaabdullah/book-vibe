23e5c1856f7b33e4a17434032fca951b340cb36e
mjaabdullah <mdjuelahmedabdullah@gmail.com>
2026-04-12 21:22:10 +0600

    Wishlist card Add

--- src/assets/Components/ListedBooks/Book.jsx
+++ src/assets/Components/ListedBooks/Book.jsx
@@ -1,3 +1,6 @@
+import { IoDocumentTextOutline } from "react-icons/io5";
+import { RiGroupLine } from "react-icons/ri";
+
 const Book = ({ book }) => {
   return (
     <div className="card card-side bg-base-100 shadow-md border border-base-200 rounded-2xl overflow-hidden">
@@ -10,37 +13,46 @@ const Book = ({ book }) => {
       </figure>
 
       <div className="card-body p-4 gap-1">
-        <h2 className="card-title text-[17px]">{book.bookName}</h2>
-        <p className="text-sm text-gray-500">By : {book.author}</p>
+        <h2 className="card-title text-[24px] font-bold">{book.bookName}</h2>
+        <p className="text-[16px] text-gray-500 font-medium">
+          By : {book.author}
+        </p>
 
         <div className="flex items-center gap-2 flex-wrap mt-1">
-          <span className="text-xs text-gray-500">Tag</span>
-          {book.tags.map((tag) => (
-            <span
-              key={tag}
-              className="badge badge-warning badge-outline text-xs"
-            >
-              #{tag}
-            </span>
-          ))}
-          <span className="text-xs text-gray-400">
-            Year: {book.yearOfPublishing}
+          <span className="flex items-center gap-2 flex-wrap font-medium">
+            <span className="text-gray-500">Tag</span>
+            {book.tags.map((tag) => (
+              <span key={tag} className="badge badge-warning badge-outline">
+                #{tag}
+              </span>
+            ))}
+          </span>
+          <span className=" text-gray-400">
+            Year of Publishing: {book.yearOfPublishing}
           </span>
         </div>
 
-        <div className="flex gap-4 flex-wrap text-xs text-gray-500">
-          <span>Publisher: {book.publisher}</span>
-          <span>Page {book.totalPages}</span>
+        <div className="mt-1 flex gap-2 flex-wrap text-gray-500">
+          <span className="flex gap-1 items-center">
+            {" "}
+            <RiGroupLine />
+            Publisher: {book.publisher}
+          </span>
+          <span className="flex gap-1 items-center">
+            {" "}
+            <IoDocumentTextOutline />
+            Page {book.totalPages}
+          </span>
         </div>
 
-        <div className="card-actions items-center mt-2 gap-2 flex-wrap">
-          <span className="badge badge-outline text-blue-500 border-blue-300 bg-blue-50">
+        <div className="card-actions items-center mt-1 gap-2 flex-wrap">
+          <span className="badge  text-blue-500  bg-blue-50">
             Category: {book.category}
           </span>
-          <span className="badge badge-outline text-orange-500 border-orange-300 bg-orange-50">
+          <span className="badge text-orange-500 bg-orange-50">
             Rating: {book.rating}
           </span>
-          <button className="btn btn-sm bg-green-500 text-white border-none rounded-full ml-auto">
+          <button className="btn btn-sm bg-green-500 text-white border-none rounded-full">
             View Details
           </button>
         </div>
