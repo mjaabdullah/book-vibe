5738d85fd63384730d220e244b8847da478cf389
mjaabdullah <mdjuelahmedabdullah@gmail.com>
2026-04-13 12:22:29 +0600

    Add Loading Spiner Styles

--- src/assets/Components/Loading/Loading.jsx
+++ src/assets/Components/Loading/Loading.jsx
@@ -1,10 +1,9 @@
 import { HashLoader } from "react-spinners";
 
 const Loading = () => {
-  console.log("Loading...");
   return (
-    <div className="flex items-center justify-center text-9xl h-screen text-red-800">
-      <HashLoader />
+    <div className="flex items-center justify-center text-9xl h-screen text-green-500">
+      <HashLoader color="#0b9b2f" size={100} />
     </div>
   );
 };
